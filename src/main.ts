35c6961dc33e6fe35fb049b37da20a8df41514f6
superdyzio <perdekdawid@gmail.com>
2021-08-24 21:18:44 +0200

    WA-4 - CardsContainer and BaseCard components added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import AppLogo from '@/components/shared/AppLogo.vue';
+import CardsContainer from '@/components/layout/CardsContainer.vue';
+import BaseCard from '@/components/shared/BaseCard.vue';
 import App from './App.vue';
 import router from './router';
 
 createApp(App)
   .component('AppLogo', AppLogo)
+  .component('CardsContainer', CardsContainer)
+  .component('BaseCard', BaseCard)
   .use(router)
   .mount('#app');
